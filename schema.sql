d65da2947609f880d8a60ffafcfe9f93213125bd
Jimmy Jaw <web3d@live.com>
2016-08-09 20:25:05 +0800

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `region` (
+    `id` int(10) unsigned NOT NULL,
+    `parent_id` int(10) unsigned NOT NULL,
+    `name` varchar(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `parent_id` (`parent_id`)
+) ENGINE=InnoDB;
